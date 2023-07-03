d86f58c22b9c5c2a6935660583e30122b9ef3608
kiatklai <likhitthaworn.k@gmail.com>
2023-07-03 16:10:22 +0900

    ルート追加/JSの編集

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -1,13 +1,50 @@
 window.addEventListener('load', function () {
 
   const parentCategory = document.getElementById('parent-category')
+  const selectWrap = document.getElementById('select-wrap')
 
   const selectChildElement = (selectForm) => {
 
   }
 
+  const XHR = new XMLHttpRequest();
+  const categoryXHR = (id) => {
+    XHR.open("GET", `/category/${id}`, true);
+    XHR.responseType = "json";
+    XHR.send();
+  }
+
+  const getChildCategoryData = () => {
+    const parentValue = parentCategory.value
+    categoryXHR(parentValue)
+
+    XHR.onload = () => {
+      const items = XHR.response.item;
+    }
+  }
+
+  const appendChildSelect = (items) => {
+
+    const childWrap = document.createElement('div')
+    const childSelect = document.createElement('select')
+
+    childWrap.setAttribute('id', 'child-select-wrap')
+    childSelect.setAttribute('id', 'child-select')
+
+    items.forEach(item => {
+      const childOption = document.createElement('option')
+      childOption.innerHTML = item.name
+      childOption.setAttribute('value', item.id)
+      childSelect.appendChild(childOption)
+    });
+
+    childWrap.appendChild(childSelect)
+    selectWrap.appendChild(childWrap)
+  }
+
   parentCategory.addEventListener('change', function () {
     selectChildElement('child-select-wrap')
+    getChildCategoryData()
   })
 
 })
